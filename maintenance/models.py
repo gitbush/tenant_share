f92a0d656ff69137963d7f8b3ffa7d4fee308e77
Matt Bush <bush.matthewjames@gmail.com>
2019-06-13 00:14:32 +0100

    Create maintenance model
    Model and first instance created and rendered through
    home view for testing purposes

--- maintenance/models.py
+++ maintenance/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Maint_request(models.Model):
+    title = models.CharField(max_length=200)
+    details = models.TextField()
+    priority = models.CharField(max_length=20)
+    status = models.CharField(max_length=20)
+    date_occurred = models.DateField()
+    image = models.ImageField()
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.title
--- maintenance/views.py
+++ maintenance/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Maint_request
 
 
 def home(request):
-    return render(request, 'maintenance/home.html')
+    requests = Maint_request.objects.all()
+    return render(request, 'maintenance/home.html', {'requests': requests})
 
 def maint_list(request):
     return HttpResponse('<h1>Maintenance List Page</h1>')
