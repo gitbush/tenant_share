b2569b3bb3ef44d8ba2c1d688e2868a4c0c49337
Matt Bush <bush.matthewjames@gmail.com>
2019-06-12 23:06:28 +0100

    Set up maintenance app
    Add app to settings.py to be able to render

--- maintenance/views.py
+++ maintenance/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def home(request):
-    return HttpResponse('<h1>Home page</h1>')
+    return render(request, 'maintenance/home.html')
 
 def maint_list(request):
     return HttpResponse('<h1>Maintenance List Page</h1>')
