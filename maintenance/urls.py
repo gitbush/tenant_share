521c412e292830e8d119b149922abff6bb108da6
Matt Bush <bush.matthewjames@gmail.com>
2019-06-12 22:41:09 +0100

    Add maintenance page
    Test in browser

--- maintenance/urls.py
+++ maintenance/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='maint-home'),
+    path('maintenance', views.maint_list, name='maint-list'),
 ]
--- maintenance/views.py
+++ maintenance/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse('<h1>Home page</h1>')
+
+def maint_list(request):
+    return HttpResponse('<h1>Maintenance List Page</h1>')
