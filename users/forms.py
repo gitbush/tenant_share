aa1e6be2b0e77f3305558fce2a0f981c2d8bc23c
Matt Bush <bush.matthewjames@gmail.com>
2019-06-15 19:56:40 +0100

    Add register as form field
    To distinguish between tenants and landlords for
    different functionality

--- users/forms.py
+++ users/forms.py
@@ -2,11 +2,16 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
+# register as field choices
+register_choices = ((None, 'Select'),('Landlord', 'Landlord'), ('Tenant', 'Tenant'))
+
+# register form
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
     first = forms.CharField(label='First Name')
     last = forms.CharField(label='Last Name')
+    register_as = forms.ChoiceField(label='Register as', choices= register_choices)
 
     class Meta:
         model = User
-        fields = ['first', 'last', 'username', 'email', 'password1', 'password2']
+        fields = ['first', 'last', 'username','register_as', 'email', 'password1', 'password2']
