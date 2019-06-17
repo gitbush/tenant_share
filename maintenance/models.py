14a802c4b5a203de570c9641db495899fb59815f
Matt Bush <bush.matthewjames@gmail.com>
2019-06-17 18:09:15 +0100

    Add rental model for storing house shares
    User will be linked to rentals as tenants or landlord

--- maintenance/models.py
+++ maintenance/models.py
@@ -1,14 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Rental(models.Model):
+    address = models.CharField(max_length=30)
+    postcode = models.CharField(max_length=6)
+    city = models.CharField(max_length=15)
+    no_of_tenants = models.IntegerField()
+    landlord = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.address
+
 class MaintRequest(models.Model):
     title = models.CharField(max_length=200)
     details = models.TextField()
-    priority = models.CharField(max_length=20)
-    status = models.CharField(max_length=20)
+    priority = models.CharField(max_length=20, default='low')
+    status = models.CharField(max_length=20, default='new')
     date_occurred = models.DateField()
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    property_ref = models.ForeignKey(Rental, null=True, on_delete=models.SET_NULL ) 
 
     def __str__(self):
         return self.title
+
+
 
