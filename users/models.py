de3a49ee25d72e8c8bfaa6ea9e197401f4eba090
Matt Bush <bush.matthewjames@gmail.com>
2019-06-17 18:33:54 +0100

    Add comments to user/models
    Code readability

--- users/models.py
+++ users/models.py
@@ -2,9 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 from maintenance.models import Rental, MaintRequest
 
+# extend django user model and add custom fields
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE) #creates relationship to user model
 
+    # custom fields
     register_as = models.CharField(max_length=10, null=True)
     rental = models.ForeignKey(Rental, null=True, on_delete=models.SET_NULL)
     requests = models.ForeignKey(MaintRequest, null=True, on_delete=models.SET_NULL)
