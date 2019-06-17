fb3bcd9e1c3e097a76e0bb6d1c3eed62646ab88b
Matt Bush <bush.matthewjames@gmail.com>
2019-06-17 18:24:57 +0100

    Extend user model with custom fields
    Register_as - landlord or tenant
    rental - house share related to user
    requests - maintenance requests authored by user

--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from maintenance.models import Rental, MaintRequest
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    register_as = models.CharField(max_length=10, null=True)
+    rental = models.ForeignKey(Rental, null=True, on_delete=models.SET_NULL)
+    requests = models.ForeignKey(MaintRequest, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+
+    
+
+
