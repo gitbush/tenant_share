f932da2ea98d17181865b78b04fca3c3dcc478a9
Matt Bush <bush.matthewjames@gmail.com>
2019-06-18 14:44:25 +0100

    Add image model for maint request and fk
    Users can up load images of defects in the house

--- maintenance/models.py
+++ maintenance/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# image model for images of maintenance requests 
+class MaintRequestImage(models.Model):
+    image = models.ImageField(default=mantenance/no_image.jpg, upload_to='maintenance')
+
+    def __str__(self):
+        return self.image
+
 # rental model for instances of rental properties
 class Rental(models.Model):
     address = models.CharField(max_length=30)
@@ -14,13 +21,15 @@ class Rental(models.Model):
 
 # maintenance request 
 class MaintRequest(models.Model):
+    property_ref = models.ForeignKey(Rental, null=True, on_delete=models.SET_NULL ) 
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    image = models.ForeignKey(MaintRequestImage, null=True, on_delete=models.SET_NULL )
     title = models.CharField(max_length=200)
     details = models.TextField()
     priority = models.CharField(max_length=20, default='low')
     status = models.CharField(max_length=20, default='new')
     date_occurred = models.DateField()
-    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
-    property_ref = models.ForeignKey(Rental, null=True, on_delete=models.SET_NULL ) 
+    
 
     def __str__(self):
         return self.title
