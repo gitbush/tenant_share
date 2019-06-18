55f14ce5ae75fb3eba2f569f1fc0482ce5eee447
Matt Bush <bush.matthewjames@gmail.com>
2019-06-18 15:06:31 +0100

    Remove requests field in profile as not needed
    Users are related to requests by author

--- users/models.py
+++ users/models.py
@@ -7,9 +7,8 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE) #creates relationship to user model
 
     # custom fields
-    register_as = models.CharField(max_length=10, null=True)
     rental = models.ForeignKey(Rental, null=True, on_delete=models.SET_NULL)
-    requests = models.ForeignKey(MaintRequest, null=True, on_delete=models.SET_NULL)
+    register_as = models.CharField(max_length=10, null=True)
     profile_image = models.ImageField(default='users/default_profile.jpg', upload_to='users')
 
     def __str__(self):
