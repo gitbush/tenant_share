438cc03b78884c9c7c56c5f753b9baf09fcaf612
Matt Bush <bush.matthewjames@gmail.com>
2019-06-21 14:30:03 +0100

    Maint_list_item links to detail view
    Current state is for template design. ID will be added to
    display relevant request

--- maintenance/urls.py
+++ maintenance/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.Home, name='maint-home'),
     path('maintenance', views.MaintRequestList, name='maint-list'),
+    path('maintenance/request', views.MaintRequestDetail, name='maint-detail'),    # edit with id when set up
 ]
