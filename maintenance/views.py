3f22a8a88c9a062cf23b10bb4485d5cd575f1efe
Matt Bush <bush.matthewjames@gmail.com>
2019-06-26 22:19:41 +0100

    Add check if current user has rental assigned
    If not then tell the user to add a rental. If so
    display the relevat rental details.
    Using basic text in no_rental.html for now

--- maintenance/views.py
+++ maintenance/views.py
@@ -5,19 +5,24 @@ from .models import *
 
 def Home(request):
     currentUser = request.user
-    userRental = currentUser.profile.rental
-    userLandlord = userRental.landlord
-    userTenants = userRental.profile_set.filter(register_as='Tenant').all()
-    # print(userRental.profile_set.filter(register_as='Tenant').all())
-    # print(currentRental.profile_set.filter(register_as='Tenant').all())
-        
+    try: 
+        userRental = currentUser.profile.rental
+        userLandlord = userRental.landlord
+        userTenants = userRental.profile_set.filter(register_as='Tenant').all()
+        # print(userRental.profile_set.filter(register_as='Tenant').all())
+        # print(currentRental.profile_set.filter(register_as='Tenant').all())
 
-    context = {
-        'userProfile': currentUser.profile,
-        'userRental': userRental, 
-        'userLandlord': userLandlord,
-        'userTenants': userTenants
-    }
+        context = {
+            'userProfile': currentUser.profile,
+            'userRental': userRental, 
+            'userLandlord': userLandlord,
+            'userTenants': userTenants
+        }
+    except:
+        context = {
+            'userProfile': currentUser.profile,
+        }
+            
     return render(request, 'maintenance/home.html', context)
 
 def MaintRequestList(request):
