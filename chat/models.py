87761b1183420f661f63e7be13d958bd5e85dcb6
Matt Bush <bush.matthewjames@gmail.com>
2019-07-03 16:28:12 +0100

    Create models for chat/messages
    Will store messages sent by users

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
+from users.models import *
+from maintenance.models import *
 
-# Create your models here.
+# model for each message thread
+class Thread(models.Model):
+    maint_request = models.ForeignKey(MaintRequest, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.maint_request.title
+
+# model for message in each thread
+class ChatMessage(models.Model):
+    thread = models.ForeignKey(Thread, null=True, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    message = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.message
