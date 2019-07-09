14c0635c1aab49120108d4bef4457c6050a17ac5
Matt Bush <bush.matthewjames@gmail.com>
2019-07-09 14:04:17 +0100

    Make add_payment form responsive
    UX/UI

--- payments/views.py
+++ payments/views.py
@@ -4,7 +4,7 @@ from .forms import PaymentForm
 
 def payments_list(request):
 
-    payment_form = PaymentForm(request.user.profile.rental, request.POST)
+    payment_form = PaymentForm(request.user.profile.rental)
 
     context = {
         'payment_form': payment_form
