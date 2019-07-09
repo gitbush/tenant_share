17dddc5c2c76a94a255d414a2270370ebcf4a5ca
Matt Bush <bush.matthewjames@gmail.com>
2019-07-09 08:43:42 +0100

    Create payment model for maint payments

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+from maintenance.models import MaintRequest, User
 
-# Create your models here.
+# maint request payments model
+class Payment(models.Model):
+    maint_request = models.ForeignKey(MaintRequest, null=True, on_delete=models.SET_NULL)
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    amount = models.IntegerField()
+    payment_date = models.DateTimeField(default=timezone.now)
+    is_paid = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.maint_request
