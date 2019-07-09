2d7d7b5ac607f5796f431b9ea1a986f583f56eb6
Matt Bush <bush.matthewjames@gmail.com>
2019-07-09 18:28:33 +0100

    Get payment history rendering in payment_list
    Shows users payment history

--- payments/views.py
+++ payments/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from .forms import PaymentForm
+from .models import Payment
 
 
 def payments_list(request):
@@ -7,19 +8,20 @@ def payments_list(request):
     List all payment history of the current user.
     Add new payment to list
     """
+    payment_list = Payment.objects.filter(user=request.user).order_by('-payment_date')
     
     if request.method == 'POST':
         payment_form = PaymentForm(request.user.profile.rental, request.POST, request.user)
         if payment_form.is_valid():
             new_payment = payment_form.save(commit=False)
             new_payment.user = request.user
-            new_payment.is_paid = True
             new_payment.save()
             return redirect('payments-list')
     else:
         payment_form = PaymentForm(request.user.profile.rental)
 
     context = {
-        'payment_form': payment_form
+        'payment_form': payment_form,
+        'payment_list': payment_list
     }
     return render(request, 'payments/payments_list.html', context)
