66ee346db19a95d8f8872a4ae8efead1f789d527
Matt Bush <bush.matthewjames@gmail.com>
2019-07-17 16:36:23 +0100

    Add  slugRelated fields to chat ser.
    Provides a means to access relationship between
    serializers

--- chat/serializers.py
+++ chat/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import ChatMessage, Thread
+from users.models import User
 
 # serializers for thread and chat models api
 class ThreadSerializer(serializers.ModelSerializer):
@@ -8,6 +9,9 @@ class ThreadSerializer(serializers.ModelSerializer):
         fields = ('id', 'rental', 'maint_request')
 
 class ChatMessageSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(many=False, slug_field='username', queryset=User.objects.all())
+    thread = serializers.SlugRelatedField(many=False, slug_field='maint_request', queryset=Thread.objects.all())
+
     class Meta:
         model = ChatMessage
         fields = ('id', 'thread', 'author', 'message', 'date_posted')
