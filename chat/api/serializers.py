9c141657a8378ea75726aa7ddbe9f6bdc79e50f3
Matt Bush <bush.matthewjames@gmail.com>
2019-07-22 17:41:22 +0100

    Add user to chat message serializer
    Can access author info

--- chat/api/serializers.py
+++ chat/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from chat.models import ChatMessage, Thread
-from users.models import User
+from users.api.serializers import UserSerializer
 
 # serializers for thread and chat models api
 class ThreadSerializer(serializers.ModelSerializer):
@@ -11,7 +11,7 @@ class ThreadSerializer(serializers.ModelSerializer):
 class ChatMessageSerializer(serializers.ModelSerializer):
     # author = serializers.SlugRelatedField(many=False, slug_field='username', queryset=User.objects.all())
     # thread = serializers.SlugRelatedField(many=False, slug_field='maint_request', queryset=Thread.objects.all())
-
+    author = UserSerializer()
     class Meta:
         model = ChatMessage
         fields = ('id','maint_request', 'author', 'message', 'date_posted')
