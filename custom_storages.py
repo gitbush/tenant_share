f572076bcd4735846e7ccc5f3db572c1d955be20
Matt Bush <bush.matthewjames@gmail.com>
2019-08-15 18:02:55 +0100

    Set-up AWS S3 bucket for static/media files
    Deploying to heroku

---
+++ custom_storages.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from storages.backends.s3boto3 import S3Boto3Storage
+
+class StaticStorage(S3Boto3Storage):
+    """
+    Create custom storage for static files
+    """
+    location = settings.STATICFILES_LOCATION
+
+class MediaStorage(S3Boto3Storage):
+    """
+    Create custom storage for media files
+    """
+    location = settings.MEDIAFILES_LOCATION
--- users/models.py
+++ users/models.py
@@ -16,15 +16,15 @@ class Profile(models.Model):
         return f'{self.user.username} Profile'
 
     #  resize uploaded profile img
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     super().save(*args, **kwargs)
 
-        img = Image.open(self.profile_image.path)
+    #     img = Image.open(self.profile_image.path)
 
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.profile_image.path)
+    #     if img.height > 300 or img.width > 300:
+    #         output_size = (300, 300)
+    #         img.thumbnail(output_size)
+    #         img.save(self.profile_image.path)
     
 
 
