d3724aa3c27ecda3ede65c75aa73f297262b7d6b
Matt Bush <bush.matthewjames@gmail.com>
2019-08-20 14:53:36 +0100

    Remove old django-imagekit template cond.
    Unused

--- users/models.py
+++ users/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.files.storage import default_storage as storage
 from maintenance.models import Rental, MaintRequest
-from imagekit.models import ProcessedImageField
-from imagekit.processors import ResizeToFill
 from PIL import Image, ImageOps
 
 # have username in uploaded image path
