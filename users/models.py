5a5f79b01407f36ecbf361aa0fa7398c7613bc70
Matt Bush <bush.matthewjames@gmail.com>
2019-08-20 14:32:07 +0100

    Modify resize image to use pillow
    Django-imagekit was causing a messy html.

--- users/models.py
+++ users/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.files.storage import default_storage as storage
 from maintenance.models import Rental, MaintRequest
 from imagekit.models import ProcessedImageField
 from imagekit.processors import ResizeToFill
-from PIL import Image
+from PIL import Image, ImageOps
 
 class Profile(models.Model):
     """
@@ -15,25 +16,40 @@ class Profile(models.Model):
     rental = models.ForeignKey(Rental, null=True, on_delete=models.SET_NULL)
     register_as = models.CharField(max_length=10, null=True)
 
+    profile_image = models.ImageField(default='users/default_profile.jpg', upload_to='users')
+
     # resize image on upload with django-imagekit
-    profile_image = ProcessedImageField(upload_to='users',
-                                           processors=[ResizeToFill(300, 300)],
-                                           format='JPEG',
-                                           options={'quality': 60})
+    # profile_image = ProcessedImageField(upload_to='users',
+    #                                        processors=[ResizeToFill(300, 300)],
+    #                                        format='JPEG',
+    #                                        options={'quality': 60})
 
     def __str__(self):
         return f'{self.user.username} Profile'
 
     #  resize uploaded profile img
-    # def save(self, *args, **kwargs):
-    #     super().save(*args, **kwargs)
-
-    #     img = Image.open(self.profile_image.path)
-
-    #     if img.height > 300 or img.width > 300:
-    #         output_size = (300, 300)
-    #         img.thumbnail(output_size)
-    #         img.save(self.profile_image.path)
+    def save(self, *args, **kwargs):
+        """
+        - installed 'django-cleanup' to auto-remove old image.
+        - installed 'pillow' to resize larger images.
+        - resizes all image formats except '.gif' as these cannot be resized
+        """
+        super(Profile, self).save(*args, **kwargs)
+        if self.profile_image:
+            img = Image.open(self.profile_image)
+            size = 300
+            thumb = (size, size)
+            method = Image.ANTIALIAS
+            center = (0.5, 0.5)
+            extension = "jpeg"
+            # if greater than 300px on any side, then resize it to 300x300
+            if img.height > size or img.width > size:
+                img.thumbnail((size, size), method)
+                new = ImageOps.fit(img, thumb, method, centering=center)
+                temp = storage.open(self.profile_image.name, "w")
+                new.save(temp, extension)
+                temp.close()
+                super(Profile, self).save(*args, **kwargs)
     
 
 
