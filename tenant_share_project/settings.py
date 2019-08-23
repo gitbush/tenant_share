8821917531d77be9a31b78c316ae54640d31440c
Matt Bush <bush.matthewjames@gmail.com>
2019-08-23 16:13:09 +0100

    Add reset link sent via gmail
    Users will get an email asking to reset their password

--- tenant_share_project/settings.py
+++ tenant_share_project/settings.py
@@ -171,5 +171,12 @@ LOGOUT_REDIRECT_URL = 'login'
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
 STRIPE_SECRET_KEY =  os.getenv('STRIPE_SECRET_KEY')
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# Passsword reset email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_ADDRESS')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD') 
+DEFAULT_FROM_EMAIL = 'support@tenantshare.com'
+EMAIL_PORT = 587
 
--- users/urls.py
+++ users/urls.py
@@ -9,10 +9,10 @@ urlpatterns = [
     path('account/<int:id>', views.account, name='account'),
     path('tenant/add/<int:rental_id>', views.add_tenant, name='add-tenant'),
     path('tenant/remove/<int:rental_id>/<int:id>', views.remove_tenant, name='remove-tenant'),
-    path('auth/password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('auth/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('auth/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('auth/password_reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('auth/password_reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset_form.html'), name='password_reset'),
+    path('auth/password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('auth/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('auth/password_reset/complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
 
 
 ]
