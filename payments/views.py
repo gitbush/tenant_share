572c783224ac1f765158dca0b2707b63c6a6717a
Matt Bush <bush.matthewjames@gmail.com>
2019-12-02 20:26:30 +0000

    Edit form fields login/register pages
    Better, cleaner ux

--- payments/views.py
+++ payments/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse 
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.core.paginator import Paginator
