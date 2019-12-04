7632ce3476a33f4b56b1c7fc9c1bc07d5af554d1
Matt Bush <bush.matthewjames@gmail.com>
2019-12-04 20:49:34 +0000

    Begin testing chat app
    Test models are created correctly

---
+++ chat/tests/test_models.py
@@ -0,0 +1,19 @@
+from django.test import TestCase, client
+from django.urls import reverse
+from chat.models import ChatMessage
+from maintenance.models import MaintRequest, Rental
+
+class TestViews(TestCase):
+
+    def setUp(self):
+        """ Create necessary instances before tests """
+
+        self.rental = Rental(address='11 Test Street')
+        self.maint_request = MaintRequest(property_ref=self.rental)
+        self.chat_message = ChatMessage(maint_request=self.maint_request, message='A test message')
+
+    def test_chat_message_is_created(self):
+        """ Test a chat message instance is created """
+        
+        self.assertEquals(self.chat_message.maint_request.id, self.maint_request.id)
+        self.assertEquals(self.chat_message.message, 'A test message')
---
+++ chat/views.py
@@ -0,0 +1,7 @@
+from django.test import TestCase, client
+from django.urls import reverse
+from .models import ChatMessage
+
+class TestViews(TestCase):
+
+    def assertEquals 1 == 2
