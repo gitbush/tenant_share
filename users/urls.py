8ea7f08df532b5f629c8cd2dce088407ea4b7642
Matt Bush <bush.matthewjames@gmail.com>
2020-02-03 06:11:05 +0000

    Make login page welcome page
    Instead of register. UX

--- users/urls.py
+++ users/urls.py
@@ -3,8 +3,8 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', views.register, name='register'),
-    path('login', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('register', views.register, name='register'),
     path('logout', auth_views.LogoutView.as_view(template_name='users/login.html'), name='logout'),
     path('account/<int:id>', views.account, name='account'),
     path('tenant/add/<int:rental_id>', views.add_tenant, name='add-tenant'),
